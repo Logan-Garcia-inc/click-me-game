150c4fa8d95407b94f8c7d902f57dffcb0dad381
25garcial <91680716+25garcial@users.noreply.github.com>
2024-10-18 11:40:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-let score = 0
-for (var i = 0; i < 100; i++) {
 
+let score = 0
+var paused=true
+for (var i = 0; i < 100; i++) {
   let spot = document.createElement("div")
   spot.classList.add("spot")
   spot.addEventListener("click", function() {
     console.log("triggered")
-    if (this.textContent == "click me") {
+    if (this.textContent == "click me" && !paused) {
+    correct()
       this.textContent = "";
       this.style.backgroundColor="pink"
-      console.log("succeded")
       score++
       document.querySelector(".score").textContent = score
       let random = Math.floor(Math.random() * 100)
        document.querySelectorAll(".spot")[random].style.backgroundColor="cyan"
       document.querySelectorAll(".spot")[random].textContent = "click me"
-      console.log(random)
+    }
+    else{
+    wrong()
     }
   })
   document.querySelector(".container").appendChild(spot)
@@ -22,5 +25,29 @@ for (var i = 0; i < 100; i++) {
 }
 let random = Math.floor(Math.random() * 100)
 document.querySelectorAll(".spot")[random].textContent = "click me"
-
 document.querySelectorAll(".spot")[random].style.backgroundColor="#00ffff"
+var startButton=document.querySelector(".start");
+async function wrong(){ new Audio('https://www.myinstants.com/media/sounds/extremely-loud-incorrect-buzzer_0cDaG20.mp3').play();}
+async function correct(){new Audio("https://github.com/Logan-Garcia-inc/click-me-game/raw/refs/heads/main/success-fanfare-trumpets-6185.mp3").play()
+}
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+var timer=document.querySelector(".timer");
+async function start(){
+if (!paused){
+return
+}
+paused=false;
+  var time=10;
+  while (time>0){
+  	time-=1
+    timer.textContent=time;
+  	await sleep(1000)
+    
+  }
+  paused=true
+}
+var startButton =document.querySelector(".start");
+startButton.addEventListener("click",start)
+
